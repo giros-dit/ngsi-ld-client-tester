34b057f5b5a308edc4817528a84bba2891cbbac3
David Martínez García <94689492+martinezgarciadavid@users.noreply.github.com>
2023-06-13 12:51:44 +0200

    Update create-sensor-entity.py and xml-parser-sensor.py
    create-sensor-entity.py -> Code modified to receive sensor data as arguments in its invocation. The invocation must be done from the xml-parser-sensor.py script.
    
    xml-parser-sensor.py -> Code modified to invocate create-sensor-entity.py passing parsed XML sensor data as arguments.

--- sensor-tester/xml-parser-sensor.py
+++ sensor-tester/xml-parser-sensor.py
@@ -4,6 +4,7 @@ Reference documentation: https://docs.python.org/3/library/xml.etree.elementtree
 '''
 
 import xml.etree.ElementTree as et
+import subprocess
 import pdb
 
 ns = {'Sensor': 'https://iot-sensor.org/iot/Sensor'}
@@ -24,6 +25,13 @@ data = {
     'humidity': humidity
 }
 
+print('SENSOR XML DATA PARSER - PARSING DATA FROM XML FILE AND CREATING NGSI-LD ENTITY')
+print('PARSED XML SENSOR DATA:')
 print(data)
+print('\n')
+
+print('CREATING NGSI-LD ENTITY GIVEN PARSED DATA...')
+# Runs create-sensor-entity.py script passing parsed XML data as arguments.
+subprocess.run(["python3.9", "create-sensor-entity.py", name, description, temperature, humidity])
 
 # pdb.set_trace()
